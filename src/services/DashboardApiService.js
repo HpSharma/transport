296db8b9d3161a710370176c26a43b73b5a00421
HpSharma <harpalhmh@gmail.com>
2025-06-20 09:59:22 -0400

    axios interceptors, base api setup

---
+++ src/services/DashboardApiService.js
@@ -0,0 +1,19 @@
+import BaseApiRequest from '@services/BaseApiRequest.js';
+
+class DashboardApiService extends BaseApiRequest {
+  constructor() {
+    super();
+  }
+
+  static fetchDashboardTilesDetails = async () => {
+    // return await BaseApiRequest.get('/dashboard/tiles');
+    return Promise.resolve({
+      vehicles: 200,
+      income: 150000,
+      transporter: 65,
+      companies: 48,
+    });
+  };
+}
+
+export default DashboardApiService;
